c28fe3e4e3d9af267d4bf661e4eee8e4e9860d4d
mgr-cse <maitreygovind@gmail.com>
2025-03-22 04:26:24 +0530

    Add plugins, theme and mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>dd", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "LSP diagnostic info" })
 
+map("n", "<M-Tab>", "<cmd> tabNext <cr>", { desc = "Next tabpage" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
